88a21d5bddb166e23c56e61f714dcb150d2a3dab
David Simba <david.simba1985@gmail.com>
2025-10-08 22:30:04 +0200

    environment variables: added

--- firebase.js
+++ firebase.js
@@ -6,14 +6,14 @@ import "firebase/compat/storage";
 //import "firebase/messaging"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCDGf-DWOM8z-I4nC2jg0PxsfLKQ_GE7o0",
-    authDomain: "notel-765b1.firebaseapp.com",
-    databaseURL: "https://notel-765b1.firebaseio.com",
-    projectId: "notel-765b1",
-    storageBucket: "notel-765b1.appspot.com",
-    messagingSenderId: "746435372425",
-    appId: "1:746435372425:web:75bd50c1e1494d3304ed58",
-    measurementId: "G-7D7WHNS6M7"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
 
   firebase.initializeApp(firebaseConfig);
